7d5c9f7510dc71836e3b8d53246d2426576ad0a2
buterrr <butergut@gmail.com>
2021-05-25 08:14:56 +0300

    Added authentication by email.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -138,6 +138,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
